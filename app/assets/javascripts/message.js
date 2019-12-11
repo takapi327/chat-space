d67ff80de1651120893960a56048f7fd81f20eed
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2019-12-11 21:52:51 +0900

    へんこう

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -20,7 +20,7 @@ $(function(){
       $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
     })
     .fail(function() {
-      console.log('error');
+      alert('error')
     });
   };
 
